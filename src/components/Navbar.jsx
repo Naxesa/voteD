529d53a2940c7433d308ae3c628b2aa6a7c7c340
Naxesa <naxesa.foundation@yahoo.com>
2025-03-18 01:15:35 +0300

    create navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link, NavLink } from "react-router-dom";
+import { IoIosMoon, IoMdSunny } from "react-icons/io";
+import { HiOutlineBars3 } from "react-icons/hi2";
+import { AiOutlineClose } from "react-icons/ai";
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+  const [darkMode, setDarkMode] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
 
-export default Navbar
+  return (
+    <div className="nav__container">
+      <Link to="/" className="nav__logo">
+        VOTE
+      </Link>
+      <div className="menu__mobile">
+        <button
+          className="theme__toggle-btn"
+          onClick={() => setMenuOpen(!menuOpen)}
+        >
+          {menuOpen ? <AiOutlineClose /> : <HiOutlineBars3 />}
+        </button>
+      </div>
+      <menu className={menuOpen ? "menu__open" : ""}>
+        <NavLink to="/elections">Elections</NavLink>
+        <NavLink to="/results">Results</NavLink>
+        <NavLink to="/logout">Logout</NavLink>
+      </menu>
+      <button
+        className="theme__toggle-btn"
+        onClick={() => setDarkMode(!darkMode)}
+      >
+        {darkMode ? <IoMdSunny /> : <IoIosMoon />}
+      </button>
+    </div>
+  );
+};
+
+export default Navbar;
