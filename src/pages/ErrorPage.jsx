c91eef0b8cd72a7841791ca18e4c2f31a1fc3fed
Naxesa <naxesa.foundation@yahoo.com>
2025-03-18 00:17:13 +0300

    finish error page

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import Image from "../assets/404.gif"; 
 
 const ErrorPage = () => {
-  return (
-    <div>ErrorPage</div>
-  )
-}
+  const navigate = useNavigate();
 
-export default ErrorPage
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      navigate(-1);
+    }, 6000);
+
+    return () => clearTimeout(timer);
+  }, [navigate]);
+
+  return (
+    <section className="errorPage__container">
+      <img src={Image} alt="Page Not Found" />
+      <h1>404</h1>
+      <p>This page does not exist. You will be redirected shortly.</p>
+    </section>
+  );
+};
+
+export default ErrorPage;
