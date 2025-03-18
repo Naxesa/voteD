ea690f7acbf3fb5336f775d7cde8087540e7a1fc
Naxesa <naxesa.foundation@yahoo.com>
2025-03-19 01:24:27 +0300

    create register page

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,9 +1,61 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
-  return (
-    <div>Register</div>
-  )
-}
+  const [userData, setUserData] = useState ({fullname:'', email:'', password:'', password2:''})
+  
+  const changeInputHandler = (e) => {
+    setUserData( prevState => {
+      return {...prevState, [e.target.name]: e.target.value}
+    })
 
-export default Register
+    console.log(userData)
+  }
+
+
+
+  return (
+    <section className="register">
+      <div className="register-container">
+        <h2>Sign Up</h2>
+        <form>
+          <input 
+            type="text" 
+            name="fullname" 
+            placeholder="Full Name" onChange={changeInputHandler}
+            className="input-field"
+          />
+          <input 
+            type="email" 
+            name="EmailAddress"  onChange={changeInputHandler}
+            placeholder="Email" 
+            className="input-field"
+          />
+          <input 
+            type="password" 
+            name="password" onChange={changeInputHandler}
+            placeholder="Password" 
+            className="input-field"
+          />
+          <input 
+            type="password" 
+            name="password2" onChange={changeInputHandler}
+            placeholder="Confirm Password" 
+            className="input-field"
+          />
+          <p className="login-link">
+            Already have an account? <Link to="/">Sign in</Link>
+          </p>
+          <button 
+            type="submit" 
+            className="register-button"
+          >
+            Register
+          </button>
+        </form>
+      </div>
+    </section>
+  );
+};
+
+export default Register;
