69f5139022d7cf487b2620ccd049591ceb0b6383
Naxesa <naxesa.foundation@yahoo.com>
2025-03-25 22:20:39 +0300

    update candidates component

--- src/components/Candidate.jsx
+++ src/components/Candidate.jsx
@@ -1,6 +1,17 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { UiActions } from '../store/ui-slice';
+import { voteActions } from '../store/vote-slice'
 
 const Candidate = ({ image, id, fullname, motto }) => {
+  const dispatch = useDispatch()
+
+   const openCandidateModal = () => {
+          dispatch(UiActions.openVoteCandidateModal())
+          dispatch(voteActions.changeSelectedVoteCandidate(id))
+  
+      }
+
   return (
     <article className="candidate">
       <div className="candidate__image">
@@ -8,7 +19,7 @@ const Candidate = ({ image, id, fullname, motto }) => {
       </div>
       <h5>{fullname?.length > 20 ? fullname.substring(0, 20) + "..." : fullname}</h5>
       <small>{motto?.length > 25 ? motto.substring(0, 25) + "..." : motto}</small>
-      <button className="btn-primary">Vote</button>
+      <button className="btn-primary" onClick={openCandidateModal}>Vote</button>
     </article>
   );
 };
